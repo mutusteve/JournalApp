1812dd49103465b573c0b43f40099be8f5d81e6b
mutusteve <stephen_mutu@yahoo.com>
2025-02-11 15:25:40 +0300

    console message to show if superbase client is initialized

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,3 +4,5 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log('Supabase client initialized:', supabase);
